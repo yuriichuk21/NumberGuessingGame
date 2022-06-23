149b975fc2250295fb16c2c9017ed56a9bec5674
yuriichuk21 <yuriichuk.cooperation@gmail.com>
2022-06-23 15:01:25 +0300

    Game is done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO.Compression;
 
 namespace NumberGuessingGame
 {
@@ -6,7 +7,32 @@ namespace NumberGuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random random = new Random();
+
+            int winNumber = random.Next(0, 10);
+
+            bool win = false;
+
+            do
+            {
+                Console.WriteLine("Guess a number in between 0 and 10");
+                string s = Console.ReadLine();
+                int i = int.Parse(s);
+
+                if (i > winNumber)
+                    Console.WriteLine("To high!  Guess lower...");
+                else if (i < winNumber)
+                    Console.WriteLine("To low!  Guess higher...");
+                else if (i == winNumber)
+                {
+                    Console.WriteLine("YOU WIN!");
+                    win = true;
+                }
+            } while (win == false);
+
+            Console.WriteLine("Thank you for playing the game!");
+            Console.Write("Press any key to finish.");
+            Console.ReadKey(true);
         }
     }
 }

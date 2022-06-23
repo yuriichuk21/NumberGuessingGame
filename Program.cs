b39a5083f3e9e9e36859112bfeb2c41ca589ee18
yuriichuk21 <yuriichuk.cooperation@gmail.com>
2022-06-23 14:41:48 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NumberGuessingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
